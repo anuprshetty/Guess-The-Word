9cf519059a077bd5d5215d2d6cf412b875dd5a07
anuprshetty <anuprshetty@gmail.com>
2015-10-23 21:02:17 +0530

    reference elements

---
+++ script.js
@@ -0,0 +1,12 @@
+const message = document.getElementById("message");
+const controls = document.querySelector(".controls-container");
+const startBtn = document.getElementById("start");
+const letterContainer = document.getElementById("letter-container");
+const userInpSection = document.getElementById("user-input-section");
+const resultText = document.getElementById("result");
+const word = document.getElementById("word");
+let randomWord = "";
+let winCount = 0;
+let lossCount = 0;
+
+
