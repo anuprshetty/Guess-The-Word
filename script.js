8253bb9743b56b317f97d2db784a810cde789b0f
anuprshetty <anuprshetty@gmail.com>
2015-11-06 19:47:30 +0530

    generate word

--- script.js
+++ script.js
@@ -18,3 +18,35 @@ startBtn.addEventListener("click", () => {
   init();
 });
 
+const generateWord = () => {
+  letterContainer.classList.remove("hide");
+  userInpSection.innerText = "";
+
+  const wordnik_api_key = "e1h2slr24u78rwf7x2dg863q52bqy96e8b2m76zf8605mcoca";
+  const url =
+    "https://api.wordnik.com/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=1000&maxCorpusCount=-1&minDictionaryCount=5&maxDictionaryCount=-1&minLength=4&maxLength=5&api_key=" +
+    wordnik_api_key;
+
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("wordnik api error");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      randomWord = data.word.replace(/[^a-zA-Z]/g, "");
+
+      let displayItem = "";
+      randomWord.split("").forEach((value) => {
+        displayItem += '<span class="inputSpace">_ </span>';
+      });
+
+      userInpSection.innerHTML = displayItem;
+      userInpSection.innerHTML += `<div id='chanceCount'>Chances Left: ${lossCount}</div>`;
+    })
+    .catch((error) => {
+      console.error("wordnik api error: ", error);
+    });
+};
+
